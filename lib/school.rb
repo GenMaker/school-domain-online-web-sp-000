a334b9bffd99dee79c1d7353d141ee90e7a8480e
Panela Leung <leungpanela@gmail.com>
2020-01-04 04:11:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,5 +1,5 @@
 require "pry"
-# code here!
+
 class School
   def initialize (school_name)
     @school_name = school_name
@@ -14,8 +14,8 @@ class School
     @student_name = name
     @grade = grade
   #binding.pry
-    if @roster.include?(grade) == false
-      @roster[grade] = []
+    roster.include?(grade) == false
+    roster[grade] = []
     end
       @roster[grade] << name
   end
@@ -29,4 +29,41 @@ class School
       @roster[grade]= name.sort
     end
   end
+end
+
+
+
+class School
+  attr_accessor :name, :roster, :mascot
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+    @mascot = []
+  end
+
+  def add_mascot(name)
+    @mascot = "#{name}"
+  end
+
+  def add_student(student_name, grade)
+  binding.pry
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  # this method should arrange the students in each grade by alphabetical order
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+
+
 end
